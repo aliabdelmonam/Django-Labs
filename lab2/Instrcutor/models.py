d72cea938fe958f0cc9254e2d55511003daf7ec8
Ali-Abdelmenam <aliabdelmonam37@gmail.com>
2026-03-15 18:44:37 +0200

    Add instructor model (without cardinality)

--- lab2/Instrcutor/models.py
+++ lab2/Instrcutor/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from utils.base_model import BaseModel
 # Create your models here.
+
+class Instructor(BaseModel):
+    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, default='Professor')
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    department = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(upload_to='instructors/', blank=True, null=True)
+    joined_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
