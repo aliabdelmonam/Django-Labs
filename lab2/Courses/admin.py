c25d7fa423c1a5ebcf30660b0e5a8d9771f9bb60
Ali-Abdelmenam <aliabdelmonam37@gmail.com>
2026-03-15 20:04:10 +0200

    ADD admin course to add through admin page

--- lab2/Courses/admin.py
+++ lab2/Courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Course
 
 # Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title', 'instructor', 'credits', 'capacity')
+    search_fields = ('code', 'title')
+    filter_horizontal = ('students',)
