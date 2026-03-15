dab64e39a2f10ce1bca8d4f3c6a3ea24c0894713
Ali-Abdelmenam <aliabdelmonam37@gmail.com>
2026-03-15 18:46:25 +0200

    Add Student model (without cardinality)

--- lab2/Students/models.py
+++ lab2/Students/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from utils.base_model import BaseModel
 # Create your models here.
+
+class Student(BaseModel):
+    name = models.CharField(max_length=200)
+    student_id = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    major = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(upload_to='students/', blank=True, null=True)
+    enrolled_date = models.DateField(auto_now_add=True)
+    gpa = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
